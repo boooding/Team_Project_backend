84fbebdaf5fad719861328f3b751f6904ac7206f
booding <booding.xiu@outlook.com>
2021-10-19 10:21:08 +0800

    add mongoose connection

---
+++ src/orm/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'user';
+
+mongoose.connect(`${url}/${dbName}`, {
+    // config
+})
+
+const db = mongoose.connection;
+
+// error handle
+db.on('error', err => {
+    console.error(err)
+})
+
+// success
+db.once('open', () => {
+    console.log('mongoose connection success')
+})
